2f006e362cef2f32367547dff2d2df4fc527bb97
Oleksandr Nepsha <oleksandr.nepsha@cashstar.com>
2020-11-09 22:29:29 +0200

    Structure changes, added formik and Yup libs.

---
+++ src/components/blocks/login-form/LoginForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import './login-form.scss';
+
+const LoginForm: React.FC = () => {
+
+  return (
+    <div>
+
+    </div>
+  )
+};
+
+export default LoginForm;
